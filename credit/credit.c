a9201f784b8bb66a84c3add70b3adcc78c6e6c5e
jessvburton <93324273+jessvburton@users.noreply.github.com>
2023-02-13 11:08:19 +0000

    Create credit.c

---
+++ credit/credit.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <cs50.h>
+int main(void)
+
+{
+    long card;
+    //get card number
+    {
+        card = get_long("card number: ");
+    }
+
+    //multiply every other number by 2 then add together
+
+    //add the answer to the digits previously excluded
+    answer = 
+
+    //PRINT ANSWER
+    if (answer = )
+    {
+        printf("AMEX");
+    }
+
+    else if (answer = )
+    {
+        printf("MASTERCARD");
+    }
+
+    else if (answer = )
+    {
+        printf("VISA");
+    }
+
+    else (answer = )
+    {
+        printf("invalid");
+    }
+}
